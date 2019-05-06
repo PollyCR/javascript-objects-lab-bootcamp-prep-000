3c262ce84f3cc0616ac7826b1f4429ae653b86e6
exuberant-object-0311 <polly.c.reynolds@gmail.com>
2019-05-06 19:43:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
 }
 
 function deleteFromObjectByKey(object,key) {
-  var newrecipes{}
+  var newrecipes{} ;
   object.assign({recipes},{newrecipes}) ;
 }
